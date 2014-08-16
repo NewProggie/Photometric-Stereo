4900dbb1c22d52839b3b6cdc5641872df2f8aa8a
Kai Wolf <kai.wolf@gmail.com>
2014-08-17 00:10:25 +0200

    Rewrite ps with dataset from Dr. Li Zhang

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <cmath>
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
@@ -7,6 +9,7 @@
 #include <vtkSmartPointer.h>
 #include <vtkPolyDataMapper.h>
 #include <vtkPolyData.h>
+#include <vtkPLYWriter.h>
 #include <vtkActor.h>
 #include <vtkProperty.h>
 #include <vtkImageViewer.h>
@@ -20,6 +23,9 @@
 #include <vtkPointData.h>
 #include <vtkFloatArray.h>
 #include <vtkTriangle.h>
+
+using namespace cv;
+using namespace std;
 
 void displayMesh(int width, int height, cv::Mat Z) {
     
@@ -66,7 +72,7 @@ void displayMesh(int width, int height, cv::Mat Z) {
     renderer->AddLight(light2);
     
     /* meshlab-ish background */
-    modelMapper->SetInput(polyData);
+    modelMapper->SetInputData(polyData);
     renderer->SetBackground(.45, .45, .9);
     renderer->SetBackground2(.0, .0, .0);
     renderer->GradientBackgroundOn();
@@ -86,6 +92,15 @@ void displayMesh(int width, int height, cv::Mat Z) {
     vtkSmartPointer<vtkRenderWindowInteractor> interactor = vtkSmartPointer<vtkRenderWindowInteractor>::New();
     interactor->SetRenderWindow(renderWindow);
     
+    /* export mesh */
+    vtkSmartPointer<vtkPLYWriter> plyExporter = vtkSmartPointer<vtkPLYWriter>::New();
+    plyExporter->SetInputData(polyData);
+    plyExporter->SetFileName("export.ply");
+    plyExporter->SetColorModeToDefault();
+    plyExporter->SetArrayName("Colors");
+    plyExporter->Update();
+    plyExporter->Write();
+    
     /* render mesh */
     renderWindow->Render();
     interactor->Start();
@@ -97,15 +112,19 @@ cv::Mat globalHeights(cv::Mat Pgrads, cv::Mat Qgrads) {
     cv::Mat Q(Pgrads.rows, Pgrads.cols, CV_32FC2, cv::Scalar::all(0));
     cv::Mat Z(Pgrads.rows, Pgrads.cols, CV_32FC2, cv::Scalar::all(0));
     
+    float lambda = 1.0f;
+    float mu = 1.0f;
+    
     cv::dft(Pgrads, P, cv::DFT_COMPLEX_OUTPUT);
     cv::dft(Qgrads, Q, cv::DFT_COMPLEX_OUTPUT);
     for (int i=0; i<Pgrads.rows; i++) {
         for (int j=0; j<Pgrads.cols; j++) {
             if (i != 0 || j != 0) {
-                float v = sin(i*2*CV_PI/Pgrads.rows);
-                float u = sin(j*2*CV_PI/Pgrads.cols);
-                float uv = u*u + v*v;
-                float d = (1+0)*uv + 0*uv*uv;
+                float u = sin((float)(i*2*CV_PI/Pgrads.rows));
+                float v = sin((float)(j*2*CV_PI/Pgrads.cols));
+                
+                float uv = pow(u,2)+pow(v,2);
+                float d = (1.0f + lambda)*uv + mu*pow(uv,2);
                 Z.at<cv::Vec2f>(i, j)[0] = (u*P.at<cv::Vec2f>(i, j)[1] + v*Q.at<cv::Vec2f>(i, j)[1]) / d;
                 Z.at<cv::Vec2f>(i, j)[1] = (-u*P.at<cv::Vec2f>(i, j)[0] - v*Q.at<cv::Vec2f>(i, j)[0]) / d;
             }
@@ -113,48 +132,120 @@ cv::Mat globalHeights(cv::Mat Pgrads, cv::Mat Qgrads) {
     }
     
     /* setting unknown average height to zero */
-    Z.at<cv::Vec2f>(0, 0)[0] = 0;
-    Z.at<cv::Vec2f>(0, 0)[1] = 0;
+    Z.at<cv::Vec2f>(0, 0)[0] = 0.0f;
+    Z.at<cv::Vec2f>(0, 0)[1] = 0.0f;
     
     cv::dft(Z, Z, cv::DFT_INVERSE | cv::DFT_SCALE | cv::DFT_REAL_OUTPUT);
     
     return Z;
 }
 
-int main() {
+cv::Vec3f getLightDirFromSphere(Mat Image, Rect boundingbox) {
     
-    /* reading images */
-    cv::Mat img1 = cv::imread("../../images/bunny_1.png", CV_LOAD_IMAGE_GRAYSCALE);
-    cv::Mat img2 = cv::imread("../../images/bunny_2.png", CV_LOAD_IMAGE_GRAYSCALE);
-    cv::Mat img3 = cv::imread("../../images/bunny_3.png", CV_LOAD_IMAGE_GRAYSCALE);
+    const int THRESH    = 254;
+    const float radius  = boundingbox.width / 2.0f;
     
+    Mat Binary;
+    threshold(Image, Binary, THRESH, 255, CV_THRESH_BINARY);
+    Mat SubImage(Binary, boundingbox);
+    
+    /* calculate center of pixels */
+    Moments m = moments(SubImage, false);
+    Point center(m.m10/m.m00, m.m01/m.m00);
+    
+    /* x,y are swapped here */
+    float x = (center.y - radius) / radius;
+    float y = (center.x - radius) / radius;
+    float z = sqrt(1.0 - pow(x, 2.0) - pow(y, 2.0));
+    
+    return Vec3f(x, y, z);
+}
+
+cv::Rect getBoundingBox(cv::Mat Mask) {
+    
+    std::vector<std::vector<cv::Point> > v;
+    cv::findContours(Mask.clone(), v, CV_RETR_LIST, CV_CHAIN_APPROX_NONE);
+    assert(v.size() > 0);
+    return cv::boundingRect(v[0]);
+}
+
+int main(int argc, char *argv[]) {
+    
+    const int NUM_IMGS          = 12;
+    const string CALIBRATION    = "../../images/chrome/chrome.";
+    const string MODEL          = "../../images/rock/rock.";
+    
+    vector<Mat> calibImages;
+    vector<Mat> modelImages;
+    Mat Lights(NUM_IMGS, 3, CV_32F);
+    Mat Mask = imread(CALIBRATION + "mask.png", CV_LOAD_IMAGE_GRAYSCALE);
+    Mat ModelMask = imread(MODEL + "mask.png", CV_LOAD_IMAGE_GRAYSCALE);
+    Rect bb = getBoundingBox(Mask);
+    for (int i = 0; i < NUM_IMGS; i++) {
+        Mat Calib = imread(CALIBRATION + to_string(i) + ".png",
+                           CV_LOAD_IMAGE_GRAYSCALE);
+        Mat tmp = imread(MODEL + to_string(i) + ".png",
+                           CV_LOAD_IMAGE_GRAYSCALE);
+        cv::Mat Model;
+        tmp.copyTo(Model, ModelMask);
+        Vec3f light = getLightDirFromSphere(Calib, bb);
+        Lights.at<float>(i, 0) = light[0];
+        Lights.at<float>(i, 1) = light[1];
+        Lights.at<float>(i, 2) = light[2];
+        calibImages.push_back(Calib);
+        modelImages.push_back(Model);
+    }
+    
+    const int height    = calibImages[0].rows;
+    const int width     = calibImages[0].cols;
     /* light directions, surface normals, p,q gradients */
-    cv::Mat Lights = (cv::Mat_<float>(3,3) << -0.2, 0.0, 1.0, 0.2, -0.2, 1.0, 0.2, 0.2, 1.0);
-    cv::Mat Normals(img1.rows, img1.cols, CV_32FC3, cv::Scalar::all(0));
-    cv::Mat Pgrads(img1.rows, img1.cols, CV_32F, cv::Scalar::all(0));
-    cv::Mat Qgrads(img1.rows, img1.cols, CV_32F, cv::Scalar::all(0));
-    
+    cv::Mat LightsInv;
+    cv::invert(Lights, LightsInv, cv::DECOMP_SVD);
+
+    cv::Mat Normals(height, width, CV_32FC3, cv::Scalar::all(0));
+    cv::Mat Pgrads(height, width, CV_32F, cv::Scalar::all(0));
+    cv::Mat Qgrads(height, width, CV_32F, cv::Scalar::all(0));
     /* estimate surface normals and p,q gradients */
-    for (int y=0; y<img1.rows; y++) {
-        for (int x=0; x<img1.cols; x++) {
-            cv::Vec3f I(img1.at<uchar>(y,x),
-                        img2.at<uchar>(y,x),
-                        img3.at<uchar>(y,x));
-            cv::Mat n = Lights.inv() * cv::Mat(I);
+    for (int x=0; x<width; x++) {
+        for (int y=0; y<height; y++) {
+            Vec<float, NUM_IMGS> I;
+            for (int i = 0; i < NUM_IMGS; i++) {
+                I[i] = modelImages[i].at<uchar>(Point(x,y));
+            }
+            
+            cv::Mat n = LightsInv * cv::Mat(I);
             float p = sqrt(cv::Mat(n).dot(n));
             if (p > 0) { n = n/p; }
             if (n.at<float>(2,0) == 0) { n.at<float>(2,0) = 1.0; }
-            Normals.at<cv::Vec3f>(cv::Point(x,y)) = n;
-            Pgrads.at<float>(cv::Point(x,y)) = -n.at<float>(0,0);
-            Qgrads.at<float>(cv::Point(x,y)) = n.at<float>(1,0);
+            int legit = 1;
+            /* avoid spikes ad edges */
+            for (int i = 0; i < NUM_IMGS; i++) {
+                legit *= modelImages[i].at<uchar>(Point(x,y)) >= 0;
+            }
+            if (legit) {
+                Normals.at<cv::Vec3f>(cv::Point(x,y)) = n;
+                Pgrads.at<float>(cv::Point(x,y)) = n.at<float>(0,0)/n.at<float>(2,0);
+                Qgrads.at<float>(cv::Point(x,y)) = n.at<float>(1,0)/n.at<float>(2,0);
+            } else {
+                cv::Vec3f nullvec(0.0f, 0.0f, 1.0f);
+                Normals.at<cv::Vec3f>(cv::Point(x,y)) = nullvec;
+                Pgrads.at<float>(cv::Point(x,y)) = 0.0f;
+                Qgrads.at<float>(cv::Point(x,y)) = 0.0f;
+            }
+            
         }
     }
+
+    cv::Mat Normalmap;
+    cv::cvtColor(Normals, Normalmap, CV_BGR2RGB);
+    cv::imshow("Normalmap", Normalmap);
     
     /* global integration of surface normals */
     cv::Mat Z = globalHeights(Pgrads, Qgrads);
-    
+
     /* display reconstruction */
     displayMesh(Pgrads.cols, Pgrads.rows, Z);
 
+    cv::waitKey();
     return 0;
 }
